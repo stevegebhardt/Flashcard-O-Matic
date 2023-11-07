097a2c81624b3d3241a2f4a20e38b12317f0bc67
stevegebhardt <gebhardt.steven3@gmail.com>
2023-11-07 10:55:31 -0500

    fixed import bug in View/index.js

--- src/View/index.js
+++ src/View/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { readDeck } from "../utils/api";
+import { deleteDeck } from "../utils/api";
 import Card from "./Card";
 
 function View() {
